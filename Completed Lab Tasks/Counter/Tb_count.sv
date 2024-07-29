dc709526c132b4c8894e09d74ff64229a86fc7e5
Hashir187 <muhammadhashir1970@gmail.com>
2024-07-30 00:29:30 +0500

    Update Tb_count.sv

--- Completed Lab Tasks/Counter/Tb_count.sv
+++ Completed Lab Tasks/Counter/Tb_count.sv
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 07/11/2024 02:16:47 PM
+// Create Date: 07/11/2024 02:13:22 PM
 // Design Name: 
 // Module Name: Tb_count
 // Project Name: 
@@ -19,28 +19,56 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/11/2024 02:13:22 PM
+// Design Name: 
+// Module Name: Tb_count
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 module Tb_count#(parameter w=4);
 
+    logic [w-1:0] a1;
+    logic sel, reset, clk;
+    logic [w-1:0] b1;
 
-logic [w-1:0]a1;
-logic sel,reset,clk;
-logic [w-1:0]b1;
+  
+    Count DUT (.a(a1), .b(b1), .clk(clk), .reset(reset), .sel(sel));
 
-Count DUT (.a(a1),.b(b1),.clk(clk),.reset(reset),.sel(sel));
-initial begin 
-        reset = 0;
-        #2 reset=~reset;
-        end
+   
+    initial begin
+        clk = 0;
+        forever #3 clk = ~clk;
+    end
 
-initial begin
-        clk=0;
-        forever #3 clk=~clk;
-        end
+  
+    initial begin
+        
+        a1 = 4'b0000;
+        sel = 1;
+        reset = 1;
+        #5 reset = 0;
 
-initial begin 
-        sel=1;
-        #100
-        $stop;
-        end
+        
+        #20 sel = 0;  
+        #20 sel = 1;  
+
+      
+        #50 $finish;
+    end
+
 endmodule
